73b10cae254e031dbf651ac508034ff9f26589b1
Przemyslaw Dabrowski <bluh@outlook.com>
2018-09-06 22:58:26 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,8 +4,13 @@ require_relative "../config/environment.rb"
 
 class Student
   attr_accessor :name, :grade
-  attr_reader :# IDEA:
+  attr_reader :id
 
+  def initialize(name, grade, id = nil)
+    @name = name
+    @grade = grade
+    @id = id
+  end
 
 
 end
