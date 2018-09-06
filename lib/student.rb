9f0c23a6aa438c7439a5f5d58e8af4b12417e684
Przemyslaw Dabrowski <bluh@outlook.com>
2018-09-06 22:57:24 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,11 @@
 require_relative "../config/environment.rb"
+# Remember, you can access your database connection anywhere in this class
+#  with DB[:conn]
 
 class Student
+  attr_accessor :name, :grade
+  attr_reader :# IDEA:
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
 
 
 end
